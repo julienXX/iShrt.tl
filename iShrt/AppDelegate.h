54b5f52b4e8fd0b471a4373b32f51cbe15f15cc8
Julien Blanchard <julien@sideburns.eu>
2011-11-06 17:32:30 +0100

    Initial Commit

---
+++ iShrt/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  iShrt
+//
+//  Created by Julien Blanchard on 06/11/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
